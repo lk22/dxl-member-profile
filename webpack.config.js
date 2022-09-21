5dd3537c413b15f479095e01b21bc3f12653d12a
Leo Knudsen <lk22>
2022-09-21 22:25:16 +0200

    reverting to previous state

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: [__dirname + "/assets/sass/app.scss"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: { outputPath: "assets/css/", name: "[name].min.css" },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
