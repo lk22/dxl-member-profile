1fc100efa8c22708fa05c1c0df20543d235b46e5
Leo Knudsen <lk22>
2022-10-17 21:37:19 +0200

    refactor ui

--- src/frontend/layout.php
+++ src/frontend/layout.php
@@ -9,7 +9,7 @@
                     <div class="col-12 col-lg-4 col-lg-3 col-xl-3 col-xxl-2 sidebar">
                         <?php require DXL_PROFILE_PARTIALS_PATH . '/sidebar.php'; ?>
                     </div>
-                    <div class="col-12 col-lg-8 rounded-md shadow-sm layout-content">
+                    <div class="col-12 col-lg-8 shadow-sm layout-content">
                         <?php 
                             if ( isset($_GET["module"]) ) {
                                 require DXL_PROFILE_VIEW_PATH . "/" . $profile["view"] . ".php";
