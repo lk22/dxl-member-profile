ec238e600ca1f8abc77f2e7e0ebd357e43cb6ff6
Leo Knudsen <lk22>
2022-12-04 15:42:13 +0100

    fix link to LAN through home view

--- src/frontend/main.php
+++ src/frontend/main.php
@@ -96,7 +96,7 @@
 									} else {
 										?>
 											<p class="text-danger">Du er ikke tilmeldt <?php echo $profile["lan"]["event"]->title ?></p>
-											<a href="/events/?action=details&type=lan&slug=<?php echo $profile["lan"]["event"]->slug ?>">Tilmeld dig <?php echo $profile["lan"]["event"]->title ?></a>
+											<a href="/events/?action=details&type=lan&event=<?php echo $profile["lan"]["event"]->slug ?>">Tilmeld dig <?php echo $profile["lan"]["event"]->title ?></a>
 										<?php
 									}
 								?>
